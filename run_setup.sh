2d9dfdfe335db3a8b95518c871a6d12ade0b89ec
Mandlenkosi <mandlenksosi.neviile@gmail.com>
2024-09-16 22:07:19 +0200

    added health check on the db service

--- run_setup.sh
+++ run_setup.sh
@@ -5,14 +5,14 @@
 set -a && source .env && set +a
 
 # Run docker compose up 
-docker-compose up --build -d 
+docker compose up --build -d 
 
 # Download WordPress
-docker-compose run --rm wp core download
+docker compose run --rm wp core download
 
 # Create WordPress Config 
-docker-compose run --rm wp config create --dbname=${MYSQL_DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=db:3306
+docker compose run --rm wp config create --dbname=${MYSQL_DB_NAME} --dbuser=${MYSQL_USER} --dbpass=${MYSQL_PASSWORD} --dbhost=db:3306
 
 # Install WordPress
-docker-compose run --rm wp core install --url=localhost:8089 --title=WPBox --admin_user=admin --admin_password=admin --admin_email=info@example.com
+docker compose run --rm wp core install --url=localhost:8089 --title=WPBox --admin_user=admin --admin_password=admin --admin_email=info@example.com
 
